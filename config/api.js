b539dd3330db33a4fba2e36c915efce3dade40d0
Rodrigo Florentino <rodrigoflorentino90@gmail.com>
2017-12-15 09:37:31 -0200

    melhoria corpo e-mail

--- config/api.js
+++ config/api.js
@@ -1,5 +1,7 @@
 require('dotenv-safe').load();
-var nodemailer = require("nodemailer");
+var nodemailer = require('nodemailer');
+var inlineCss = require('nodemailer-juice');
+
 var usuario = process.env.MAIL_USER;
 var senha = process.env.MAIL_PASSWORD;
 var enviado = 'Mister Xper '
@@ -14,11 +16,14 @@ var smtpTransport = nodemailer.createTransport({
     }
 });
 
+smtpTransport.use('compile', inlineCss())
+
 var correio = {
 
     enviaCorreio: function (req, res, next) {
 
         var enviarP = '';
+        var css = '';
 
         if(req.body.sendTo == '-'){
             enviarP = usuario
@@ -32,14 +37,14 @@ var correio = {
             to: enviarP,
             subject: req.body.subject,
             text: req.body.vtext,
-            html: req.body.vhtml
+            html: css + req.body.vhtml
         }
 
         smtpTransport.sendMail(mailOptions, function (error, response) {
             if (error) {
                 res.status(500).send(error);
             } else {
-                res.status(200).send("Message sent: " + JSON.stringify(response));
+                res.status(200).send('Message sent: ' + JSON.stringify(response));
             }
         });
     },
