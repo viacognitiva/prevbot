9f8f33a51810b3c83a3aafebd470eefa90fc4645
Rodrigo Florentino <rodrigo.florentino@vbofficeware.com.br>
2018-08-29 13:48:43 -0300

    alteração validação

--- app/core/fim/fimController.js
+++ app/core/fim/fimController.js
@@ -10,7 +10,7 @@
 
             var vm          = this;
             vm.restartChat  = restartChat;
-            vm.messageExit = $localStorage.dados.nome + ', a ViaCognitiva agradece sua visita e fica à sua disposição.';
+            vm.messageExit = $localStorage.dados.nome + ', a ViaCognitiva agradece a sua visita e fica à sua disposição.';
 
 
            function restartChat() {
